552e0a51b694a3be22357ed3e23a3d94bf7e93bc
Aletia88 <abababmgmgmg@gmail.com>
2025-02-04 23:48:56 +0300

    fix: logo

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -29,19 +29,19 @@ const Aside: React.FC<Props> = ({ activeNavItem, onNavItemClick }) => {
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
-        <div className="-mx-6 px-6 py-4">
+        {/* <div className="-mx-6 px-6 py-4">
           <a href="#" title="home">
             <img
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
+              src="/photo_2024-05-24_08-02-13.jpg"
               className="w-32"
               alt="tailus logo"
             />
           </a>
-        </div>
+        </div> */}
 
         <div className="mt-8 text-center">
           <img
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src="/Group 52.png"
             alt=""
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
